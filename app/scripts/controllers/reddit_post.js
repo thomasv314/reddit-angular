fc1a809b8f5fa5b2ff93fc317a6ef9f6686bd0bb
Thomas Vendetta <thomas@thomasvendetta.com>
2013-11-23 18:30:09 -0500

    Added html into comments and selftext

--- app/scripts/controllers/reddit_post.js
+++ app/scripts/controllers/reddit_post.js
@@ -12,9 +12,27 @@ angular.module('redditApp')
     $window.location = $scope.post.url;
   };
 
+  $scope.htmlDecode = function(input) {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+  }
+
+  $scope.commentHtml = function(comment) {
+    return $scope.htmlDecode(comment.body_html);
+  };
+
   $http.get($scope.post_url).success(function(data) {
+    // Set the Post
     $scope.post = data[0].data.children[0].data;
+    // Compute the date of the Post
     $scope.post.date_short = new Date($scope.post.created*1000).formatShort(); 
+    // Set the Post Text
+    if ($scope.post.selftext_html) {
+      $scope.selftext = $scope.htmlDecode($scope.post.selftext_html);
+      console.log($scope.selftext); 
+    }
+    // Parse the array of comments
     $scope.post_comments = data[1].data.children.map(function(obj) {
       obj.data.date = new Date(obj.data.created*1000);
       obj.data.date_short = obj.data.date.formatShort();
@@ -25,11 +43,4 @@ angular.module('redditApp')
     console.log($scope);
   });   
 
-  $scope.postHtml = function(post) {
-    return $sce.trustAsHtml(post.selftext_html);
-  }
-
-  $scope.commentHtml = function(comment) {
-    return $sce.trustAsHtml(comment.body_html);
-  }
 });
