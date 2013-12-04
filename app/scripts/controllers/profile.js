594bdb176fc9af1b9937e7a5d8ac3b7efd020f42
Thomas Vendetta <thomas@thomasvendetta.com>
2013-12-04 11:24:33 -0500

    Profile page

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,18 +1,27 @@
 'use strict';
 
 angular.module('redditApp')
-  .controller('ProfileCtrl', function ($scope, $routeParams, Reddit) {
+  .controller('ProfileCtrl', function ($scope, $routeParams, $http, Reddit) {
     
     $scope.username = $routeParams.username;
-    
-    var onUserSuccess = function(data) {
-     console.log("loaded user...", data);
-    }
-
-    var onUserError = function(data, status, headers, config) {
-      alert("Error retrieving user information on profile.");
-    }
    
-    Reddit.getUserInfo($scope.username, onUserSuccess, onUserError);
+  
+    // Load the user's information 
+    $scope.loaded_user_info = false;
+    $scope.error_loading_user_info = false;
+
+    $scope.successLoadingUserInfo = function(data) {
+      $scope.loaded_user_info = true;
+    };
+
+    $scope.errorLoadingUserInfo = function(data, status, headers, config) {
+      $scope.error_loading_user_info = true; 
+    };
+
+    $http.get(Reddit.getUrl('user/'+$scope.username+'.json'))
+    .success($scope.successLoadingUserInfo)
+    .error($scope.errorLoadingUserInfo)
+
+    $scope.loaded_user_history = false;
 
   });
